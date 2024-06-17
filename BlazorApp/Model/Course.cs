4e7e319e3d521059d8a2eb647cc0e7412ae6a049
Ákos Klobetz <klobetz@gmail.com>
2024-06-17 17:38:19 +0200

    # adatok megjelenítése

--- BlazorApp/Model/Course.cs
+++ BlazorApp/Model/Course.cs
@@ -3,7 +3,7 @@
     public class Course
     {
         public int CourseId { get; set; }
-        public string? Name { get; set; }
+        public string? CourseName { get; set; }
 
         //kapcsolat kiépítése
         public virtual List<Student> Students { get; set; } = new List<Student>();
